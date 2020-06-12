0fd0a34f0e97c3aefe71c89671428ce14df4e5a1
Michael Hladun <mp.hladun@gmail.com>
2020-06-12 12:30:39 -0400

    feat: readme preview, fix: page numbering

--- art_app/views.py
+++ art_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from .models import Post
 
-PAGE_POSTS = 15
+PAGE_POSTS = 1
 
 def index(request):
     """Shows posts numbered 0 to PAGE_POSTS"""
@@ -11,18 +11,18 @@ def index(request):
 
 def index_page(request, page: int = 0):
     """Shows posts numbered N to N + PAGE_POSTS"""
-    posts = Post.objects.all()
-    post_count = posts.count()
+    posts = Post.objects.order_by('-post_pub_date')
+    posts_len = posts.count()
 
     lower = page*PAGE_POSTS
     upper = lower + PAGE_POSTS
 
-    if page*PAGE_POSTS > post_count or page < 0:
-        return redirect('art_app:index_page')
+    if page*PAGE_POSTS >= posts_len or page < 0:
+        return redirect('art_app:index')
 
     post_list = posts[lower : upper]
     prev_page = page-1 if page >= 1 else None
-    next_page = page+1 if post_count > page*PAGE_POSTS + PAGE_POSTS else None
+    next_page = page+1 if posts_len > page*PAGE_POSTS + PAGE_POSTS else None
 
     pages = {'prev': prev_page, 'next': next_page}
     context = {'post_list': post_list, 'pages': pages}
